5f406704406253a8a4f3e9f00346e2e8f96d7651
Rohan Kumar <therohankumar7@gmail.com>
2020-02-10 14:07:03 -0600

    setup.py for installing

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requires = [
+    'pandas==0.25.3',
+]
+
+setuptools.setup(
+    name='pandaSQL',
+    version='0.0.1',
+    author="Rohan Kumar",
+    author_email="rk@rohankumar.io",
+    description="Pandas-style Data Analysis with SQLite3 Backend",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rohankumar42/pandaSQL",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+        "Operating System :: OS Independent",
+        "Development Status :: 1 - Planning",
+    ],
+    install_requires=requires,
+    python_requires='>=3.6',
+)
