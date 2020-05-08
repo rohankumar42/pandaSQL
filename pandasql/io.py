4bc98187a97aabfd9a8b43e7e07a633343c3526e
Rohan Kumar <therohankumar7@gmail.com>
2020-05-07 22:40:15 -0500

    read_csvs function

--- pandasql/io.py
+++ pandasql/io.py
@@ -1,19 +1,24 @@
 import pandas as pd
 
-from pandasql.core import DataFrame
+import pandasql as ps
 
 
 def read_csv(*args, name=None, **kwargs):
-    return DataFrame(pd.read_csv(*args, **kwargs), name=name)
+    return ps.DataFrame(pd.read_csv(*args, **kwargs), name=name)
+
+
+def read_csvs(files, *args, name=None, **kwargs):
+    return ps.concat([read_csv(f, *args, name=name, **kwargs)
+                      for f in files])
 
 
 def read_json(*args, name=None, **kwargs):
-    return DataFrame(pd.read_json(*args, **kwargs), name=name)
+    return ps.DataFrame(pd.read_json(*args, **kwargs), name=name)
 
 
 def read_numpy(*args, name=None, **kwargs):
-    return DataFrame(pd.read_numpy(*args, **kwargs), name=name)
+    return ps.DataFrame(pd.read_numpy(*args, **kwargs), name=name)
 
 
 def read_pickle(*args, name=None, **kwargs):
-    return DataFrame(pd.read_pickle(*args, **kwargs), name=name)
+    return ps.DataFrame(pd.read_pickle(*args, **kwargs), name=name)
