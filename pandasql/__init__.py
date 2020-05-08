8af339852292c84eeabbb9f553e6dc62073a3ee1
Rohan Kumar <therohankumar7@gmail.com>
2020-05-07 22:40:15 -0500

    read_csvs function

--- pandasql/__init__.py
+++ pandasql/__init__.py
@@ -1,11 +1,11 @@
 import os
 from .core import DataFrame, stop, offloading_strategy, concat, merge  # noqa
-from .io import read_csv, read_json, read_numpy, read_pickle  # noqa
+from .io import read_csv, read_csvs, read_json, read_numpy, read_pickle  # noqa
 
 __all__ = [
     'DataFrame', 'stop', 'offloading_strategy'
     'concat', 'merge',
-    'read_csv', 'read_json', 'read_numpy', 'read_pickle'
+    'read_csv', 'read_csvs', 'read_json', 'read_numpy', 'read_pickle'
 ]
 
 offloading_strategy(os.getenv('PANDASQL_OFFLOADING') or 'ALWAYS')
