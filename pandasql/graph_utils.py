1e2d18e7797b5e6c3b7a8235957fe57369fa7792
Rohan Kumar <therohankumar7@gmail.com>
2020-08-11 13:37:11 -0500

    renamed utils to graph utils

---
+++ pandasql/graph_utils.py
@@ -0,0 +1,78 @@
+from queue import Queue
+from collections import defaultdict
+
+
+def _get_dependency_graph(table):
+    dependencies = {}
+
+    def add_dependencies(child):
+        dependencies[child] = list(child.sources)
+        for parent in dependencies[child]:
+            if parent not in dependencies:
+                add_dependencies(parent)
+
+    add_dependencies(table)
+    return dependencies
+
+
+def _topological_sort(graph):
+    # Reversed graph: node to children that depend on it
+    parent_to_child = defaultdict(list)
+    for child, parents in graph.items():
+        for parent in parents:
+            parent_to_child[parent].append(child)
+
+    ready = Queue()  # Nodes that have 0 dependencies
+    num_deps = {node: len(deps) for (node, deps) in graph.items()}
+    for node, count in num_deps.items():
+        if count == 0:
+            ready.put(node)
+    if ready.empty():
+        raise ValueError('Cyclic dependency graph! Invalid computation.')
+
+    results = []    # All nodes in order, with dependencies before dependents
+    while not ready.empty():
+        node = ready.get()
+        results.append(node)
+
+        # Update dependency counts for all dependents of node
+        for child in parent_to_child[node]:
+            num_deps[child] -= 1
+            if num_deps[child] == 0:  # Child has no more dependencies
+                ready.put(child)
+
+    if len(results) != len(graph):
+        raise RuntimeError('Expected {} nodes but could only sort {}'
+                           .format(len(graph), len(results)))
+
+    return results
+
+
+def _filter_ancestors(df, graph, predicate, max_depth=None):
+    ancestors_by_depth = _get_ancestors_by_depth(df, graph, max_depth)
+    filtered = []
+    for ancestors in ancestors_by_depth.values():
+        filtered.extend(filter(predicate, ancestors))
+    return filtered
+
+
+def _get_ancestors_by_depth(df, graph, max_depth=None):
+    max_depth = len(graph) if max_depth is None else max_depth
+
+    visited = {df}
+    ancestors_by_depth = {0: {df}}
+
+    depth = 0
+    while depth < max_depth and \
+            len(ancestors_by_depth[depth]) > 0:  # There are unexplored nodes
+
+        cur_layer = ancestors_by_depth[depth]
+        depth += 1
+        ancestors_by_depth[depth] = set()
+        for cur in cur_layer:
+            for neighbor in graph[cur]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    ancestors_by_depth[depth].add(neighbor)
+
+    return ancestors_by_depth
---
+++ tests/test_graph_utils.py
@@ -0,0 +1,43 @@
+import unittest
+import pandas as pd
+import pandasql as ps
+from pandasql.graph_utils import _get_dependency_graph, _topological_sort
+
+
+class TestGraphUtils(unittest.TestCase):
+
+    def test_get_dependency_graph(self):
+        base_df_1 = pd.DataFrame([{'n': i, 's1': str(i*2)} for i in range(10)])
+        df_1 = ps.DataFrame(base_df_1)
+        base_df_2 = pd.DataFrame([{'n': i, 's2': str(i*2)} for i in range(10)])
+        df_2 = ps.DataFrame(base_df_2)
+        merged = df_1.merge(df_2, on='n')
+
+        graph = _get_dependency_graph(merged)
+        self.assertIn(df_1, graph)
+        self.assertIn(df_2, graph)
+        self.assertIn(merged, graph)
+        self.assertIn(df_1, set(graph[merged]))
+        self.assertIn(df_2, set(graph[merged]))
+        self.assertEqual(len(graph[merged]), 2)
+        self.assertEqual(len(graph[df_1]), 0)
+        self.assertEqual(len(graph[df_2]), 0)
+
+    def test_topological_sort(self):
+        base_df_1 = pd.DataFrame([{'n': i, 's1': str(i*2)} for i in range(10)])
+        df_1 = ps.DataFrame(base_df_1)
+        base_df_2 = pd.DataFrame([{'n': i, 's2': str(i*2)} for i in range(10)])
+        df_2 = ps.DataFrame(base_df_2)
+        merged = df_1.merge(df_2, on='n')
+
+        graph = _get_dependency_graph(merged)
+        ordered = _topological_sort(graph)
+        self.assertEqual(ordered[0].name, df_1.name)
+        self.assertEqual(ordered[1].name, df_2.name)
+        self.assertEqual(ordered[2].name, merged.name)
+
+    # TODO: test other graph utils
+
+
+if __name__ == "__main__":
+    unittest.main()
